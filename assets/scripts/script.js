e7594ddb45cc275d6de48ec53c3dda8cdb42b8c5
Rabbok <rabbokk@gmail.com>
2023-02-21 12:17:19 +0200

    fix: fixed bug with script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,16 @@
-let calculatorNavButtons = document.getElementsByClassName('calculator-nav__button');
+const calculatorNavButtons = document.querySelectorAll('.calculator-nav__button');
+const calculatorBlocks = document.querySelectorAll('.changes-block');
+
+let activeBlock = 0;
+
+const reset = () => {
+    calculatorBlocks.forEach(calculatorBlock => calculatorBlock.style.display = 'none');
+    calculatorBlocks[activeBlock].style.display = 'grid';
+}
+
+calculatorNavButtons.forEach((calculatorNavButton, index) => {
+    calculatorNavButton.addEventListener('click', () => {
+        activeBlock = index;
+        reset();
+    });
+});

72d54eb1520b18521e3993c2b5a5ed2faa643aca
Rabbok <rabbokk@gmail.com>
2023-04-14 11:15:06 +0300

    fix: fixed HTML, CSS and JavaScript issues

--- assets/scripts/visual.js
+++ assets/scripts/visual.js
@@ -1,23 +1,28 @@
-const visual_calculatorNavButtons = document.querySelectorAll('.calculator-nav__button');
-const visual_calculatorBlocks = document.querySelectorAll('.changes-block');
-const visual_changeBlock = document.querySelector('.character-change');
-const visual_statsChangeButtons = document.querySelectorAll('.stats__button');
-const visual_teamButtons = document.querySelectorAll('.open-button');
-const visual_teamBlocks = document.querySelectorAll('.team-stats__block');
-const visual_artifactBlocks = document.querySelectorAll('.artifact-set');
-const visual_charcterButton = document.querySelectorAll('.character-weapon__button');
-const visual_character = document.querySelectorAll(".character-change__character");
-const visual_statsFrom = document.querySelectorAll('.stats-form');
-
-let visual_activeBlock = 0;
-let visual_activeTeamBlock = 0;
-let visual_activeArtBlock = 0;
+const visual = {
+    calculatorNavButtons: document.querySelectorAll('.calculator-nav__button'),
+    calculatorBlocks: document.querySelectorAll('.changes-block'),
+    changeBlock: document.querySelector('.character-change'),
+    statsChangeButtons: document.querySelectorAll('.stats__button'),
+    teamButtons: document.querySelectorAll('.open-button'),
+    teamBlocks: document.querySelectorAll('.team-stats__block'),
+    artifactBlocks: document.querySelectorAll('.artifact-set'),
+    charcterButton: document.querySelectorAll('.character-weapon__button'),
+    character: document.querySelectorAll(".character-change__character"),
+    statsFrom: document.querySelectorAll('.stats-form'),
+    artifactChangeButton: document.querySelectorAll('.art-change__button'),
+    activeBlock: 0,
+    activeTeamBlock: 0,
+    activeArtBlock: 0
+}
 
 //Function for changing parameters in the stats block
 
-const visual_statsButtonHandler = (event) => {
+const statsButtonHandler = (event) => {
     const parentElement = event.target.parentNode;
     parentElement.querySelectorAll('.stats__button').forEach((item) => {
+        item.classList.remove('active-element');
+    })
+    parentElement.querySelectorAll('.art-change__button').forEach((item) => {
         item.classList.remove('active-element');
     })
     event.target.classList.add('active-element');
@@ -25,74 +30,78 @@ const visual_statsButtonHandler = (event) => {
 
 //Function for changing calculator blocks
 
-const visual_reset = () => {
-    visual_calculatorBlocks.forEach(calculatorBlock => calculatorBlock.classList.remove('active-block'));
-    visual_calculatorBlocks[visual_activeBlock].classList.add('active-block');
-    visual_changeBlock.classList.remove('active-block');
+const reset = () => {
+    visual.calculatorBlocks.forEach(calculatorBlock => calculatorBlock.classList.remove('active-block'));
+    visual.calculatorBlocks[visual.activeBlock].classList.add('active-block');
+    visual.changeBlock.classList.remove('active-block');
 }
 
 //Functions for changing character
 
-const visual_characterChange = () => {
-    visual_changeBlock.classList.add('active-block');
-    visual_calculatorBlocks[visual_activeBlock].classList.remove('active-block');
+const characterChange = () => {
+    visual.changeBlock.classList.add('active-block');
+    visual.calculatorBlocks[visual.activeBlock].classList.remove('active-block');
 }
 
-const visual_characterChangeReset = () => {
-    visual_changeBlock.classList.remove('active-block');
-    visual_calculatorBlocks[visual_activeBlock].classList.add('active-block');
+const characterChangeReset = () => {
+    visual.changeBlock.classList.remove('active-block');
+    visual.calculatorBlocks[visual.activeBlock].classList.add('active-block');
 }
 
 //Functions for team stats
 
-const visual_teamStatsHendler = () => {
-    visual_teamBlocks[visual_activeTeamBlock].classList.contains('active-block') ? visual_teamBlocks[visual_activeTeamBlock].classList.remove('active-block') : visual_teamBlocks[visual_activeTeamBlock].classList.add('active-block');
+const teamStatsHendler = () => {
+    visual.teamBlocks[visual.activeTeamBlock].classList.toggle('active-block');
 }
 
 //Function for changing artifact blocks
 
-const visual_artifactHandler = () => {
-    visual_artifactBlocks[visual_activeArtBlock].classList.contains('active-element') ? visual_artifactBlocks[visual_activeArtBlock].classList.remove('active-element') : visual_artifactBlocks[visual_activeArtBlock].classList.add('active-element');
-    visual_artifactBlocks[visual_activeArtBlock].querySelectorAll('.artifact-description').forEach((deskBlock) => {
-        deskBlock.classList.remove('active-element');;
+const artifactHandler = () => {
+    visual.artifactBlocks[visual.activeArtBlock].classList.toggle('active-element');
+    visual.artifactBlocks[visual.activeArtBlock].querySelectorAll('.artifact-set__description').forEach((deskBlock) => {
+        deskBlock.classList.remove('active-element');
     });
 }
 
-visual_calculatorNavButtons.forEach((item, index) => {
+visual.calculatorNavButtons.forEach((item, index) => {
     item.addEventListener('click', () => {
-        visual_activeBlock = index;
-        visual_reset()
+        visual.activeBlock = index;
+        reset()
     })
 })
 
-visual_charcterButton.forEach((item) => {
-    item.addEventListener('click', visual_characterChange);
+visual.charcterButton.forEach((item) => {
+    item.addEventListener('click', characterChange);
 });
 
-visual_character.forEach((item) => {
-    item.addEventListener('click', visual_characterChangeReset);
+visual.character.forEach((item) => {
+    item.addEventListener('click', characterChangeReset);
 });
 
-visual_statsFrom.forEach((item) => {
+visual.character.forEach((item) => {
     item.addEventListener('submit', (event) => {
         event.preventDefault();
     });
 })
 
-visual_statsChangeButtons.forEach((item) => {
-    item.addEventListener('click', visual_statsButtonHandler);
+visual.statsChangeButtons.forEach((item) => {
+    item.addEventListener('click', statsButtonHandler);
 })
 
-visual_teamButtons.forEach((teamButton, index) => {
+visual.artifactChangeButton.forEach((item) => {
+    item.addEventListener('click', statsButtonHandler);
+})
+
+visual.teamButtons.forEach((teamButton, index) => {
     teamButton.addEventListener('click', () => {
-        visual_activeTeamBlock = index;
-        visual_teamStatsHendler();
+        visual.activeTeamBlock = index;
+        teamStatsHendler();
     })
 })
 
-visual_artifactBlocks.forEach((artifactBlock, index) => {
+visual.artifactBlocks.forEach((artifactBlock, index) => {
     artifactBlock.addEventListener('click', () => {
-        visual_activeArtBlock = index;
-        visual_artifactHandler();
+        visual.activeArtBlock = index;
+        artifactHandler();
     })
 })
